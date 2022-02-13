988fac72c8a0f0c64f9033ff73876945c146653e
loloy03 <78993871+loloy03@users.noreply.github.com>
2022-02-13 17:43:39 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,10 @@
+from base64 import decode
+from ctypes.wintypes import PINT
+
+
 print('hello world')
+deci = str(input('pogi ba si ed?:[y/n]'))
+if deci == 'y':
+    print('tama ka pogi si eds!')
+else:
+    print('oops yari ka kay eds!')
